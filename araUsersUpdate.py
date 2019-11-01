f19a55d743e3589b9109dbb791e26ef24fc69495
EmanuelBarth <50994065+EmanuelBarth@users.noreply.github.com>
2019-11-01 09:59:48 +0100

    User IDs are now automatically checked and updated to ARA.

--- araUsersUpdate.py
+++ araUsersUpdate.py
@@ -1,8 +1,21 @@
-#!/home/mu42cuq/python_3.6/Python-3.6.0/bin/python3.6
+#!/home/mu42cuq/programs/Python-3.6.2/bin/python3.6
 #-*- coding: utf-8 -*-
 
 import pickle
 import subprocess
+import re
 
-#araUsers = pickle.load(open('/home/mu42cuq/araUsers.pydict', 'rb'))
-#test = subprocess.getoutput('ssh mu42cuq@ara-login01.rz.uni-jena.de squeue | iconv --to-code utf-8//IGNORE') 
+araUsers = pickle.load(open('/home/mu42cuq/scripts/araUsers.pydict', 'rb'))
+
+newDict = {}
+for userID in set(re.findall('[a-z][a-z]\d\d[a-z][a-z][a-z]', subprocess.getoutput('ssh mu42cuq@ara-login01.rz.uni-jena.de squeue | iconv --to-code utf-8//IGNORE'))):
+    print(f'Checking User {userID}', end='\r')
+    try:
+        newDict[userID] = subprocess.getoutput(f'finger {userID}').split('\t\t\tName: ')[1].split('\n')[0]
+    except:
+        newDict[userID] = userID
+print('')
+araUsers.update(newDict)
+pickle.dump(araUsers, open('/home/mu42cuq/scripts/araUsers.pydict', 'wb'))
+if subprocess.getstatusoutput('scp /home/mu42cuq/scripts/araUsers.pydict ara-login01.rz.uni-jena.de:/home/mu42cuq/araUsers.pydict')[0] != 0:
+    print('Could not save araUsers.pydict to ARA. Something wrong with the connection?')
