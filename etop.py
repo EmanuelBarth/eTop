d5da1ca82e5c188aa22e80141fc3cd5d8cdcb541
EmanuelBarth <emanuel.barth@hotmail.de>
2019-11-01 10:01:07 +0100

    Removed some outfated system calls

--- etop.py
+++ etop.py
@@ -4,13 +4,10 @@
 import time
 import pickle
 import subprocess
-from random import randint
 
 araUsers = pickle.load(open('/home/mu42cuq/araUsers.pydict', 'rb'))
-
 partition = {x: {'tot': 0, 'run': 0, 'pen': 0} for x in
              ['s_standard', 's_fat', 'b_standard', 'b_fat', 'gpu_p100', 'gpu_v100']}
-
 currUsers = {}
 
 lastNodeName = ''
@@ -19,7 +16,6 @@ for lin in subprocess.getoutput('scontrol show partition').split('\n'):
         lastNodeName = lin.split('PartitionName=')[1].strip()
     elif 'TotalNodes=' in lin and lastNodeName in partition:
         partition[lastNodeName]['tot'] = int(lin.split('TotalNodes=')[1].split()[0])
-
 
 for lin in subprocess.getoutput('squeue | iconv --to-code utf-8//IGNORE').split('\n')[1:]:
     nil = lin.strip().split()
@@ -37,7 +33,6 @@ for lin in subprocess.getoutput('squeue | iconv --to-code utf-8//IGNORE').split(
             partition[nil[1]]['pen'] += 1
 
 print('\n\n¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯\n')
-
 print(f'{time.asctime()}\n')
 print('partition\t\trunning\t\t\tpending')
 print('---------\t\t-------\t\t\t-------')
@@ -61,7 +56,3 @@ for usr in sorted(currUsers):
             print("", end='\t')
     print(f"{araUsers.get(usr, usr)[:20]:<20}")
 print('\n¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯\n\n')
-
-
-
-#test = subprocess.getoutput('ssh mu42cuq@ara-login01.rz.uni-jena.de squeue | iconv --to-code utf-8//IGNORE') 
