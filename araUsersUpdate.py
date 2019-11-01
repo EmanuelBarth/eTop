c084403d1c5fe519cf736c268c76218e3efacb69
EmanuelBarth <50994065+EmanuelBarth@users.noreply.github.com>
2019-11-01 08:33:40 +0100

    Create araUsersUpdate.py
    Helper script to update the ARA users login names to real names.

---
+++ araUsersUpdate.py
@@ -0,0 +1,8 @@
+#!/home/mu42cuq/python_3.6/Python-3.6.0/bin/python3.6
+#-*- coding: utf-8 -*-
+
+import pickle
+import subprocess
+
+#araUsers = pickle.load(open('/home/mu42cuq/araUsers.pydict', 'rb'))
+#test = subprocess.getoutput('ssh mu42cuq@ara-login01.rz.uni-jena.de squeue | iconv --to-code utf-8//IGNORE') 
