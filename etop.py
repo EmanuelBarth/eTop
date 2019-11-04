bc5bbd412ba4009dd4a016b62a8aacffc4a65647
EmanuelBarth <emanuel.barth@hotmail.de>
2019-11-04 09:02:07 +0100

    Added some comments.

--- etop.py
+++ etop.py
@@ -1,23 +1,22 @@
 #!/home/mu42cuq/python_3.6/Python-3.6.0/bin/python3.6
 #-*- coding: utf-8 -*-
-
 import time
 import pickle
 import subprocess
 
 araUsers = pickle.load(open('/home/mu42cuq/araUsers.pydict', 'rb'))
 partition = {x: {'tot': 0, 'run': 0, 'pen': 0} for x in
-             ['s_standard', 's_fat', 'b_standard', 'b_fat', 'gpu_p100', 'gpu_v100']}
+             ['s_standard', 's_fat', 'b_standard', 'b_fat', 'gpu_p100', 'gpu_v100']} #predefined partition list; if more partitions are added to ARA, this list has to be extended
 currUsers = {}
 
 lastNodeName = ''
-for lin in subprocess.getoutput('scontrol show partition').split('\n'):
+for lin in subprocess.getoutput('scontrol show partition').split('\n'):     #This Loop gets all the installed partitions and the corresponding available notes
     if 'PartitionName=' in lin:
         lastNodeName = lin.split('PartitionName=')[1].strip()
-    elif 'TotalNodes=' in lin and lastNodeName in partition:
+    elif 'TotalNodes=' in lin and lastNodeName in partition:                #However, only nodes from the predefined partition list are stored, to exclude all the test_nodes, which can not be accessed by the normal users anyway
         partition[lastNodeName]['tot'] = int(lin.split('TotalNodes=')[1].split()[0])
 
-for lin in subprocess.getoutput('squeue | iconv --to-code utf-8//IGNORE').split('\n')[1:]:
+for lin in subprocess.getoutput('squeue | iconv --to-code utf-8//IGNORE').split('\n')[1:]:      #get the current running and queued jobs along with the respective user IDs; the iconv command is needed if some dumbass uses some weird non-Unicode code points in his/her job names
     nil = lin.strip().split()
     if 'standar' in nil[1]:
         nil[1] = f'{nil[1]}d'
@@ -43,7 +42,7 @@ for par in ['s_standard', 's_fat', 'b_standard', 'b_fat', 'gpu_p100', 'gpu_v100'
     else:
         print(f"{par}\t\t{partition[par]['run']}/{partition[par]['tot']} ({round(partition[par]['run']/partition[par]['tot']*100,1)}%)\t\t{partition[par]['pen']}")
 
-print('\n\nID\t|s_std\ts_fat\tb_std\tb_fat\tGp100\tGv100\tuser name')
+print('\n\nID\t|s_std\ts_fat\tb_std\tb_fat\tGp100\tGv100\tuser name')   #These two print commands could be generalized to avoid manual correction if new partitions are added to ARA... but at the moment I'm to lazy to do it...
 print('--\t|-----\t-----\t-----\t-----\t-----\t-----\t---------')
 
 for usr in sorted(currUsers):
@@ -54,5 +53,5 @@ for usr in sorted(currUsers):
             print(f"({currUsers[usr][par]['pen']})", end='\t')
         else:
             print("", end='\t')
-    print(f"{araUsers.get(usr, usr)[:20]:<20}")
+    print(f"{araUsers.get(usr, usr)[:20]:<20}")         #Cut user names to 20 characters, for style reasons
 print('\n¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯¯\_(ツ)_/¯\n\n')
